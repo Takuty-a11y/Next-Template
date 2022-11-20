34c5b0bf1897d50458421f18c07d875b4b9dcba9
Takuty-a11y <takuty.008.awenite.1121@gmail.com>
2022-11-20 23:05:17 +0900

    Add Wrapper Components

--- src/types/style.d.ts
+++ src/types/style.d.ts
@@ -2,7 +2,7 @@
 /**
  * Responsiveプロパティ
  */
- export type ResponsiveProp<T> = {
+export type ResponsiveProp<T> = {
   base?: T
   sm?: T
   md?: T
@@ -10,3 +10,101 @@
   xl?: T
 }
 export type Responsive<T> = T | ResponsiveProp<T>
+
+type CSSPropertyGlobals =
+  | '-moz-initial'
+  | 'inherit'
+  | 'initial'
+  | 'revert'
+  | 'unset'
+
+type SelfPosition =
+  | 'center'
+  | 'end'
+  | 'flex-end'
+  | 'flex-start'
+  | 'self-end'
+  | 'self-start'
+  | 'start'
+
+type ContentDistribution =
+  | 'space-around'
+  | 'space-between'
+  | 'space-evenly'
+  | 'stretch'
+
+type ContentPosition = 'center' | 'end' | 'flex-end' | 'flex-start' | 'start'
+
+export type CSSPropertyAlignItems =
+  | CSSPropertyGlobals
+  | SelfPosition
+  | 'baseline'
+  | 'normal'
+  | 'stretch'
+  // コードの自動補完
+  | (string & {})
+
+export type CSSPropertyAlignContent =
+  | CSSPropertyGlobals
+  | ContentDistribution
+  | 'center'
+  | 'end'
+  | 'flex-end'
+  | 'flex-start'
+  | 'start'
+  | 'baseline'
+  | 'normal'
+  | (string & {})
+
+export type CSSPropertyJustifyContent =
+  | CSSPropertyGlobals
+  | ContentDistribution
+  | ContentPosition
+  | 'left'
+  | 'normal'
+  | 'right'
+  | (string & {})
+
+export type CSSPropertyJustifyItems =
+  | CSSPropertyGlobals
+  | SelfPosition
+  | 'baseline'
+  | 'left'
+  | 'legacy'
+  | 'normal'
+  | 'right'
+  | 'stretch'
+  | (string & {})
+
+export type CSSPropertyFlexWrap =
+  | CSSPropertyGlobals
+  | 'nowrap'
+  | 'wrap'
+  | 'wrap-reverse'
+
+export type CSSPropertyFlexDirection =
+  | CSSPropertyGlobals
+  | 'column'
+  | 'column-reverse'
+  | 'row'
+  | 'row-reverse'
+
+export type CSSPropertyJustifySelf =
+  | CSSPropertyGlobals
+  | SelfPosition
+  | 'auto'
+  | 'baseline'
+  | 'left'
+  | 'normal'
+  | 'right'
+  | 'stretch'
+  | (string & {})
+
+export type CSSPropertyAlignSelf =
+  | CSSPropertyGlobals
+  | SelfPosition
+  | 'auto'
+  | 'baseline'
+  | 'normal'
+  | 'stretch'
+  | (string & {})
--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,29 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
-import { theme } from "themes";
-import { Responsive, ResponsiveProp } from "types/style";
+import { theme } from 'themes'
+import { Responsive, ResponsiveProp } from 'types/style'
 
-type AppTheme = typeof theme;
-type SpaceThemeKeys = keyof typeof theme.space;
-type ColorThemeKeys = keyof typeof theme.colors;
-type FontSizeThemeKeys = keyof typeof theme.fontSizes;
-type LetterSpacingThemeKeys = keyof typeof theme.letterSpacings;
-type LineHeightThemeKeys = keyof typeof theme.lineHeights;
+type AppTheme = typeof theme
+type SpaceThemeKeys = keyof typeof theme.space
+type ColorThemeKeys = keyof typeof theme.colors
+type FontSizeThemeKeys = keyof typeof theme.fontSizes
+type LetterSpacingThemeKeys = keyof typeof theme.letterSpacings
+type LineHeightThemeKeys = keyof typeof theme.lineHeights
 
 //外部参照用のtheme型
-export type Space = SpaceThemeKeys | (string & {});
-export type Color = ColorThemeKeys | (string & {});
-export type FontSize = FontSizeThemeKeys | (string & {});
-export type LetterSpacing = LetterSpacingThemeKeys | (string & {});
-export type LineHeight = LineHeightThemeKeys | (string & {});
+export type Space = SpaceThemeKeys | (string & {})
+export type Color = ColorThemeKeys | (string & {})
+export type FontSize = FontSizeThemeKeys | (string & {})
+export type LetterSpacing = LetterSpacingThemeKeys | (string & {})
+export type LineHeight = LineHeightThemeKeys | (string & {})
 
 // ブレイクポイント
 const BREAKPOINTS: { [key: string]: string } = {
-  sm: "640px", // 640px以上
-  md: "768px", // 768px以上
-  lg: "1024px", // 1024px以上
-  xl: "1280px", // 1280px以上
-};
+  sm: '640px', // 640px以上
+  md: '768px', // 768px以上
+  lg: '1024px', // 1024px以上
+  xl: '1280px', // 1280px以上
+}
 
 /**
  * Responsive型をCSSプロパティとその値に変換
@@ -35,59 +35,59 @@ const BREAKPOINTS: { [key: string]: string } = {
 export const toPropValue = <T>(
   propKey: string,
   prop?: Responsive<T>,
-  theme?: AppTheme
+  theme?: AppTheme,
 ) => {
-  if (prop === undefined) return undefined;
+  if (prop === undefined) return undefined
 
   if (isResponsiveType(prop)) {
-    const result = [];
+    const result = []
     for (const responseKey in prop) {
-      if (responseKey === "base") {
+      if (responseKey === 'base') {
         result.push(
           `${propKey}: ${toThemeValueIfNeeded(
             propKey,
             prop[responseKey],
-            theme
-          )}`
-        );
+            theme,
+          )}`,
+        )
       } else if (
-        responseKey === "sm" ||
-        responseKey === "md" ||
-        responseKey === "lg" ||
-        responseKey === "xl"
+        responseKey === 'sm' ||
+        responseKey === 'md' ||
+        responseKey === 'lg' ||
+        responseKey === 'xl'
       ) {
         // メディアクエリでのスタイル
-        const breakpoint = BREAKPOINTS[responseKey];
+        const breakpoint = BREAKPOINTS[responseKey]
         const style = `${propKey}: ${toThemeValueIfNeeded(
           propKey,
           prop[responseKey],
-          theme
-        )};`;
-        result.push(`@media screen and (min-width: ${breakpoint}) {${style}}`);
+          theme,
+        )};`
+        result.push(`@media screen and (min-width: ${breakpoint}) {${style}}`)
       }
     }
-    return result.join("\n");
+    return result.join('\n')
   }
 
-  return `${propKey}: ${toThemeValueIfNeeded(propKey, prop, theme)};`;
-};
+  return `${propKey}: ${toThemeValueIfNeeded(propKey, prop, theme)};`
+}
 
 const SPACE_KEYS = new Set([
-  "margin",
-  "margin-top",
-  "margin-left",
-  "margin-bottom",
-  "margin-right",
-  "padding",
-  "padding-top",
-  "padding-left",
-  "padding-bottom",
-  "padding-right",
-]);
-const COLOR_KEYS = new Set(["color", "background-color"]);
-const FONT_SIZE_KEYS = new Set(["font-size"]);
-const LINE_SPACING_KEYS = new Set(["letter-spacing"]);
-const LINE_HEIGHT_KEYS = new Set(["line-height"]);
+  'margin',
+  'margin-top',
+  'margin-left',
+  'margin-bottom',
+  'margin-right',
+  'padding',
+  'padding-top',
+  'padding-left',
+  'padding-bottom',
+  'padding-right',
+])
+const COLOR_KEYS = new Set(['color', 'background-color'])
+const FONT_SIZE_KEYS = new Set(['font-size'])
+const LINE_SPACING_KEYS = new Set(['letter-spacing'])
+const LINE_HEIGHT_KEYS = new Set(['line-height'])
 
 /**
  * Themeに指定されたCSSプロパティの値に変換
@@ -99,7 +99,7 @@ const LINE_HEIGHT_KEYS = new Set(["line-height"]);
 const toThemeValueIfNeeded = <T>(
   propKey: string,
   value: T,
-  theme?: AppTheme
+  theme?: AppTheme,
 ) => {
   if (
     theme &&
@@ -107,38 +107,38 @@ const toThemeValueIfNeeded = <T>(
     SPACE_KEYS.has(propKey) &&
     isSpaceThemeKeys(value, theme)
   ) {
-    return theme.space[value];
+    return theme.space[value]
   } else if (
     theme &&
     theme.colors &&
     COLOR_KEYS.has(propKey) &&
     isColorThemeKeys(value, theme)
   ) {
-    return theme.colors[value];
+    return theme.colors[value]
   } else if (
     theme &&
     theme.fontSizes &&
     FONT_SIZE_KEYS.has(propKey) &&
     isFontSizeThemeKeys(value, theme)
   ) {
-    return theme.fontSizes[value];
+    return theme.fontSizes[value]
   } else if (
     theme &&
     theme.letterSpacings &&
     LINE_SPACING_KEYS.has(propKey) &&
     isLetterSpacingThemeKeys(value, theme)
   ) {
-    return theme.letterSpacings[value];
+    return theme.letterSpacings[value]
   } else if (
     theme &&
     theme.lineHeights &&
     LINE_HEIGHT_KEYS.has(propKey) &&
     isLineHeightThemeKeys(value, theme)
   ) {
-    return theme.lineHeights[value];
+    return theme.lineHeights[value]
   }
-  return value;
-};
+  return value
+}
 
 function isResponsiveType<T>(prop: any): prop is ResponsiveProp<T> {
   return (
@@ -148,41 +148,41 @@ function isResponsiveType<T>(prop: any): prop is ResponsiveProp<T> {
       prop.md !== undefined ||
       prop.lg !== undefined ||
       prop.xl != undefined)
-  );
+  )
 }
 
 const isSpaceThemeKeys = (
   prop: any,
-  theme: AppTheme
+  theme: AppTheme,
 ): prop is SpaceThemeKeys => {
-  return Object.keys(theme.space).filter((key) => key == prop).length > 0;
-};
+  return Object.keys(theme.space).filter((key) => key == prop).length > 0
+}
 const isColorThemeKeys = (
   prop: any,
-  theme: AppTheme
+  theme: AppTheme,
 ): prop is ColorThemeKeys => {
-  return Object.keys(theme.colors).filter((key) => key == prop).length > 0;
-};
+  return Object.keys(theme.colors).filter((key) => key == prop).length > 0
+}
 
 const isFontSizeThemeKeys = (
   prop: any,
-  theme: AppTheme
+  theme: AppTheme,
 ): prop is FontSizeThemeKeys => {
-  return Object.keys(theme.fontSizes).filter((key) => key == prop).length > 0;
-};
+  return Object.keys(theme.fontSizes).filter((key) => key == prop).length > 0
+}
 
 const isLetterSpacingThemeKeys = (
   prop: any,
-  theme: AppTheme
+  theme: AppTheme,
 ): prop is LetterSpacingThemeKeys => {
   return (
     Object.keys(theme.letterSpacings).filter((key) => key == prop).length > 0
-  );
-};
+  )
+}
 
 const isLineHeightThemeKeys = (
   prop: any,
-  theme: AppTheme
+  theme: AppTheme,
 ): prop is LineHeightThemeKeys => {
-  return Object.keys(theme.lineHeights).filter((key) => key == prop).length > 0;
-};
+  return Object.keys(theme.lineHeights).filter((key) => key == prop).length > 0
+}
